03f9a5f05992fac91c6bbd45df7a1013fccd2852
ayushd49 <yesnowhy709@gmail.com>
2026-04-06 09:03:02 -0400

    docker

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello from Docker on EC2!</h1><p>Running inside a container on AWS.</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
