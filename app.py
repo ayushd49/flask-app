a80c0f12e830a631b1a0f34309ff225aea864e41
ayushd49 <yesnowhy709@gmail.com>
2026-04-07 10:49:45 -0400

    v13

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "<h1>Hello from Docker on EC2!------re---------last</h1><p>Running inside a container on AWS.</p>"
+def home():
+    return "<h1>Hello, Flask!</h1><p>My first Flask app.</p>"
 
 if __name__ == "__main__":
-    
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
