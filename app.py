008273cdea8d1fac0f01a5ed1f39b240d81000f1
ayushd49 <yesnowhy709@gmail.com>
2026-04-07 08:08:20 -0400

    v6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello from Docker on EC2!------re---------</h1><p>Running inside a container on AWS.</p>"
+    return "<h1>Hello from Docker on EC2!------re---------12</h1><p>Running inside a container on AWS.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
