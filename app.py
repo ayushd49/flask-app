fe1b2404f1dae91087ea7c1a00e954ae42d687f6
ayushd49 <yesnowhy709@gmail.com>
2026-04-13 08:00:21 -0400

    poll

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, Flask1001!</h1><p>My first Flask app.</p>"
+    return "<h1>Hello, Flask90000!</h1><p>My first Flask app.</p>"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=5000)
